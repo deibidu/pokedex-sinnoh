d3dbaebe2be50a57fa981a5d890b87e716b999d8
deibidu <davidlopezfaustino@gmail.com>
2024-07-22 10:10:49 +0200

    rectificacion de vercel y routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import App from "./App";
 import { Home } from "./pages/Home";
 import Favoritos from "./pages/Favoritos";
-import { PokemonList } from "./utils/fetchPokemon";
+import PokemonList from "./utils/fetchPokemon";
 import { createBrowserRouter } from "react-router-dom";
 
 export const routes = createBrowserRouter([
